e84bb02ad98e9e086aa8811e86240c5d68c8074e
Rachel Carden <Rachel.Carden@disney.com>
2018-04-12 21:52:02 -0700

    Optimizing the build

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,112 @@
-// Require all the things (that we need).
-var autoprefixer = require('gulp-autoprefixer');
-var gulp = require('gulp');
-var minify = require('gulp-minify');
-var phpcs = require('gulp-phpcs');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp');
+const mergeMediaQueries = require('gulp-merge-media-queries');
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+const shell = require('gulp-shell');
+const sort = require('gulp-sort');
+const uglify = require('gulp-uglify');
+const wp_pot = require('gulp-wp-pot');
 
 // Define the source paths for each file type.
-var src = {
-	scss: ['assets/scss/*.scss'],
-	js: ['assets/js/*.js','!assets/js/*.min.js'],
-	php: ['**/*.php','!vendor/**','!node_modules/**']
+const src = {
+	js: ['assets/js/my-multi-authors-admin.js'],
+	php: ['**/*.php','!vendor/**','!node_modules/**'],
+	sass: ['assets/scss/*.scss']
 };
 
 // Define the destination paths for each file type.
-var dest = {
-	scss: './assets/css',
-	js: './assets/js'
+const dest = {
+	js: './assets/js',
+	sass: './assets/css',
+	translations: 'languages/my-multi-author.pot'
 };
 
-// Sass is fun.
+// Take care of SASS.
 gulp.task('sass', function() {
-	return gulp.src(src.scss)
+	return gulp.src(src.sass)
 		.pipe(sass({
-			outputStyle: 'compressed'
-		})
-		.on('error', sass.logError))
+			outputStyle: 'expanded'
+		}).on('error', sass.logError))
+		.pipe(mergeMediaQueries())
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(gulp.dest(dest.scss));
-});
-
-// Minify the JS.
-gulp.task('js',function() {
-	gulp.src(src.js)
-		.pipe(minify({
-			mangle: false,
-			ext:{
-				min:'.min.js'
-			}
+		.pipe(cleanCSS({
+			compatibility: 'ie8'
 		}))
-		.pipe(gulp.dest(dest.js))
-});
-
-// Sniff our code.
-gulp.task('php',function () {
-	return gulp.src(src.php)
-		.pipe(phpcs({
-			bin: './vendor/bin/phpcs',
-			standard: 'WordPress-Core'
+		.pipe(rename({
+			suffix: '.min'
 		}))
-		// Log all problems that was found
-		.pipe(phpcs.reporter('log'));
+		.pipe(gulp.dest(dest.sass))
+		.pipe(notify('My Multi Author SASS compiled'));
 });
 
 // Make sure autocomplete files are copied.
 gulp.task('autocomplete', function () {
 	gulp.src('./node_modules/select2/dist/css/select2.min.css')
-		.pipe(gulp.dest(dest.scss));
+		.pipe(gulp.dest(dest.sass));
 	gulp.src('./node_modules/select2/dist/js/select2.min.js')
-			.pipe(gulp.dest(dest.js));
+		.pipe(gulp.dest(dest.js));
 });
 
-// Let's get this party started.
-gulp.task('default',['autocomplete','compile','test']);
+// Minify the JS.
+gulp.task('js', function() {
+	gulp.src(src.js)
+		.pipe(uglify({
+			mangle: false
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest(dest.js))
+		.pipe(notify('My Multi Author JS compiled'));
+});
+
+// "Sniff" our PHP.
+gulp.task('php', function() {
+	// TODO: Clean up. Want to run command and show notify for sniff errors.
+	return gulp.src('my-multi-author-plugin.php', {read: false})
+		.pipe(shell(['composer sniff'], {
+			ignoreErrors: true,
+			verbose: false
+		}))
+		.pipe(notify('My Multi Author PHP sniffed'), {
+			onLast: true,
+			emitError: true
+		});
+});
+
+// Create the .pot translation file.
+gulp.task('translate', function () {
+	gulp.src('**/*.php')
+		.pipe(sort())
+		.pipe(wp_pot( {
+			domain: 'my-multi-author',
+			package: 'my-multi-author-plugin',
+			bugReport: 'https://github.com/bamadesigner/my-multi-author-plugin/issues',
+			lastTranslator: 'Rachel Cherry <bamadesigner@gmail.com>',
+			headers: false
+		} ))
+		.pipe(gulp.dest(dest.translations))
+		.pipe(notify('My Multi Author translated'));
+});
+
+// Test our files.
+gulp.task('test',['php']);
 
 // Compile all the things.
 gulp.task('compile',['sass','js']);
 
-// Test all the things.
-gulp.task('test',['php']);
-
 // I've got my eyes on you(r file changes).
-gulp.task('watch',function() {
-	gulp.watch(src.scss,['sass']);
-	gulp.watch(src.js,['js']);
-	gulp.watch(src.php,['php']);
+gulp.task('watch', function() {
+	gulp.watch(src.js, ['js']);
+	gulp.watch(src.php,['php','translate']);
+	gulp.watch(src.sass,['sass']);
 });
+
+// Let's get this party started.
+gulp.task('default', ['autocomplete','compile','translate','test','watch']);
